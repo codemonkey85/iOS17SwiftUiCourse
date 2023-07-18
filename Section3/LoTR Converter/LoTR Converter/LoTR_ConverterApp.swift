518e1109791e96d8dbf97e7eaaf781f3d74b5327
Michael Bond <michael@bondcodes.com>
2023-07-18 16:03:59 -0400

    Create first project, add assets

---
+++ Section3/LoTR Converter/LoTR Converter/LoTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoTR_ConverterApp.swift
+//  LoTR Converter
+//
+//  Created by Michael Bond on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LoTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
